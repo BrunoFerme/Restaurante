1aee6093e7e9831d43b15f37763709f5277f7f6e
Bruno Fermé <95652123+BrunoFerme@users.noreply.github.com>
2022-05-19 16:56:28 +0100

    Add files via upload

---
+++ src/views/AboutView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <ul>
+    <li v-for='(prato,index) in refeicoes' :key="index">
+      {{prato.strCategory}}
+      <img :src="prato.strCategoryThumb" alt="">
+       </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+   return{
+     refeicoes: ['hamburguer', 'ovo frito']
+   } 
+  },
+  mounted (){
+    this.axios.get('https://www.themealdb.com/api/json/v1/1/categories.php')
+    .then(
+      res => {
+        console.log(res)
+        this.refeicoes=res.data.categories
+      }
+
+    )
+  }
+}
+</script>
